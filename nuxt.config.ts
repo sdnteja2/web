414cad4ba9e7fec269cd85637edb6d466600be5b
Dinar, Permadi <dinarpermadi07@gmail.com>
2024-01-01 03:41:07 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,49 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/fontaine',
+  ],
+  ui: {
+    global: true,
+    icons: ['solar'],
+  },
+  googleFonts: {
+    display: 'swap',
+    prefetch: true,
+    preconnect: true,
+    preload: true,
+    families: {
+      'Rethink Sans': {
+        wght: '400..800',
+        ital: '400..800',
+      },
+      'Work Sans': {
+        wght: '100..900',
+        ital: '100..900',
+      },
+    },
+  },
+  fontMetrics: {
+    fonts: [
+      {
+        family: 'Rethink Sans',
+        fallbacks: ['Rethink Sans'],
+        fallbackName: 'Rethink Sans',
+        src: 'fonts/Rethink_Sans-400_800-1.woff2',
+        root: 'assets',
+      },
+      {
+        family: 'Work Sans',
+        fallbacks: ['Work Sans'],
+        fallbackName: 'Work Sans',
+        src: 'fonts/Work_Sans-100_900-5.woff2',
+        root: 'assets',
+      },
+    ],
+  },
+
+})
---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+/* eslint-disable ts/no-require-imports */
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+export default <Partial<Config>> {
+
+  theme: {
+
+    extend: {
+      colors: {
+        merah: {
+          DEFAULT: '#F22727',
+          50: '#FCD4D4',
+          100: '#FBC1C1',
+          200: '#F99A9A',
+          300: '#F77474',
+          400: '#F44D4D',
+          500: '#F22727',
+          600: '#D40D0D',
+          700: '#9F0A0A',
+          800: '#6A0606',
+          900: '#350303',
+          950: '#1B0202',
+        },
+      },
+      fontFamily: {
+        body: ['Rethink Sans', 'Roboto fallback', ...fontFamily.sans],
+        title: ['Work Sans', 'Inter fallback', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+    './app.vue',
+    './content/**/*.md',
+  ],
+}
