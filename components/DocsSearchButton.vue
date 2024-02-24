fa323baa83b265cc0783c1b7b11b18ae6603587c
Dinar, Permadi <dinarpermadi07@gmail.com>
2024-02-25 05:09:05 +0700

    Update icons and descriptions in content pages

--- components/DocsSearchButton.vue
+++ components/DocsSearchButton.vue
@@ -1,33 +1,140 @@
 <script setup lang="ts">
-const { isSearchModalOpen } = useDocs()
-const { metaSymbol } = useShortcuts()
+const isOpen = ref(false)
+
+const router = useRouter()
+const commandPaletteRef = ref()
+
+const { data: files } = await useLazyAsyncData('search', () => queryContent().where({ _type: 'markdown' }).find(), { default: () => [] })
+
+const defaultGroups = computed(() => files.value.map(file => ({
+  id: file._id,
+  title: file.title,
+  to: file._path,
+  suffix: file.description,
+  icon: file.icon,
+})))
+
+const queryGroups = computed(() => files.value.flatMap((file) => {
+  return [{
+    id: file._id,
+    title: file.title,
+    to: file._path,
+    description: file.description,
+    icon: file.icon,
+  },
+
+  // @ts-expect-error broken
+  ...Object.entries(groupByHeading(file.body.children)).map(([hash, { title, children }]) => ({
+    id: `${file._path}${hash}`,
+    title,
+    prefix: `${file.title} ->`,
+    prefixClass: 'text-gray-700 dark:text-gray-200',
+    to: `${file._path}${hash}`,
+    children: concatChildren(children),
+    icon: file.icon,
+  }))]
+}))
+const groups = computed(() =>
+  [commandPaletteRef.value?.query
+    ? {
+        key: 'queryGroups',
+        commands: queryGroups.value,
+      }
+    : {
+        key: 'defaultGroups',
+        commands: defaultGroups.value,
+      }].filter(Boolean))
+// const groups = computed(() => commandPaletteRef.value?.query ? queryGroups.value : defaultGroups.value)
+
+function remapChildren(children: any[]) {
+  return children?.map((grandChild) => {
+    if (['code-inline', 'em', 'a', 'strong'].includes(grandChild.tag))
+      return { type: 'text', value: grandChild.children.find((child: any) => child.type === 'text')?.value || '' }
+
+    return grandChild
+  })
+}
+
+function concatChildren(children: any[]) {
+  return children.map((child: any) => {
+    if (['alert'].includes(child.tag))
+      child.children = concatChildren(child.children)
+
+    if (child.tag === 'p') {
+      child.children = remapChildren(child.children)
+
+      child.children = child.children?.reduce((acc: any[], grandChild: any) => {
+        if (grandChild.type === 'text') {
+          if (acc.length && acc[acc.length - 1].type === 'text')
+            acc[acc.length - 1].value += grandChild.value
+          else
+            acc.push(grandChild)
+        }
+        else {
+          acc.push(grandChild)
+        }
+        return acc
+      }, [])
+    }
+    if (['style'].includes(child.tag))
+      return null
+
+    return child
+  })
+}
+function groupByHeading(children: any[]) {
+  const groups: { [key: string]: { children: any[], title: string | null } } = {} // grouped by path
+  let hash = '' // file.page with potential `#anchor` concat
+  let title: string | null = null
+  for (const node of children) {
+    // if heading found, udpate current path
+    if (['h2', 'h3'].includes(node.tag)) {
+      // find heading text value
+      title = node.children?.find((child: any) => child.type === 'text')?.value
+      if (title)
+        hash = `#${node.props.id}`
+    }
+    // push to existing/new group based on path
+    if (groups[hash])
+      groups[hash].children.push(node)
+    else
+      groups[hash] = { children: [node], title }
+  }
+  return groups
+}
+
+function onSelect(option: any) {
+  if (option.click)
+    option.click()
+  else if (option.to)
+    router.push(option.to)
+  else if (option.href)
+    window.open(option.href, '_blank')
+}
 </script>
 
 <template>
-  <UButton
-    color="white"
-    variant="outline"
-    icon="i-heroicons-magnifying-glass-20-solid"
-    label="Search..."
-    truncate
-    :ui="{
-      color: {
-        white: {
-          outline: 'ring-1 ring-inset ring-gray-200 dark:ring-gray-800 hover:ring-gray-300 dark:hover:ring-gray-700 hover:bg-gray-50 dark:hover:bg-gray-800/50 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400',
-        },
-      },
-    }"
-    @click="isSearchModalOpen = true"
-  >
-    <template #trailing>
-      <div class="hidden lg:flex items-center gap-0.5 ml-auto -my-1 flex-shrink-0">
-        <UKbd class="!text-inherit">
-          {{ metaSymbol }}
-        </UKbd>
-        <UKbd class="!text-inherit">
-          K
-        </UKbd>
-      </div>
-    </template>
-  </UButton>
+  <div>
+    <UButton label="Open" @click="isOpen = true" />
+    <UModal v-model="isOpen">
+      <!-- <pre> {{ queryGroups }}</pre> -->
+      <UCommandPalette
+        command-attribute="title"
+        :groups="groups"
+        :close-button="{ icon: 'i-heroicons-x-mark-20-solid', color: 'gray', variant: 'ghost', size: 'sm' }"
+        :fuse="{
+          fuseOptions: {
+            ignoreLocation: true,
+            includeMatches: true,
+            threshold: 0,
+            keys: ['title', 'description', 'children.children.value', 'children.children.children.value'],
+          },
+          resultLimit: 10,
+        }"
+
+        @update:model-value="onSelect"
+        @click="isOpen = false"
+      />
+    </UModal>
+  </div>
 </template>
